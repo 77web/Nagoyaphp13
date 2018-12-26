2289a1716e84e7dd2bd7b872e5d0c15878de9c7c
77web <info@77-web.com>
2018-12-26 19:52:19 +0900

    列を下にシフトする処理

--- src/CommandRunner/ColumnDown.php
+++ src/CommandRunner/ColumnDown.php
@@ -7,21 +7,21 @@ namespace Nanaweb\Nagoyaphp13\CommandRunner;
 use Nanaweb\Nagoyaphp13\Box;
 use Nanaweb\Nagoyaphp13\Command;
 
-class ColumnUp implements CommandRunnerInterface
+class ColumnDown implements CommandRunnerInterface
 {
     public function run(Box $box, Command $command): Box
     {
         $column = $box->getColumn($command->getTargetNumber());
-        $column[0]->setIndex($column[0]->getIndex() + 6);
-        $column[1]->setIndex($column[1]->getIndex() - 3);
-        $column[2]->setIndex($column[2]->getIndex() - 3);
+        $column[0]->setIndex($column[0]->getIndex() + 3);
+        $column[1]->setIndex($column[1]->getIndex() + 3);
+        $column[2]->setIndex($column[2]->getIndex() - 6);
 
         return $box->updateMap($column);
     }
 
     public function supports(Command $command): bool
     {
-        return $command->getTargetType() === Command::TARGET_TYPE_COLUMN && $command->getDirection() === Command::DIRECTION_UP;
+        return $command->getTargetType() === Command::TARGET_TYPE_COLUMN && $command->getDirection() === Command::DIRECTION_DOWN;
     }
 
 }
